cbf09f240db7beca52f20223ea846922a17ff98c
Ravindu9EV <yravindu18@gmail.com>
2024-07-07 13:24:22 +0530

    published

---
+++ app.js
@@ -0,0 +1,68 @@
+// https://restcountries.com/v3.1/all
+
+// let res = fetch("https://restcountries.com/v3.1/all").then((res) => {
+//   console.log(res);
+// });
+
+let tblCountries = document.getElementById("tblCountries");
+
+let tableBody = `
+     <tr>
+        <th>Name</th>
+        <th>Flag</th>
+      </tr>
+`;
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      tableBody += `
+         <tr>
+            <td><h1>${element.name.common}</h1> <br>
+            Official Name : ${element.name.official}
+            <p>Region : ${element.region}</p>
+            <p>Population : ${element.population} </p>
+            <p>Area : ${element.area} 
+            <p>Time Zone : ${element.timezones}</p>
+            <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Map</a>
+
+            </td>
+            <td><img src="${element.flags.png}" alt=""></td>
+        </tr>
+        `;
+
+      console.log(element.name.common);
+    });
+
+    tblCountries.innerHTML = tableBody;
+  });
+
+function searchCountry() {
+  let userInput = document.getElementById("txtInput").value;
+
+  let flagImg = document.getElementById("flagImg");
+  let name = document.getElementById("name");
+  let officialName = document.getElementById("officialName");
+  let region = document.getElementById("region");
+  let population = document.getElementById("population");
+  let area = document.getElementById("area");
+  let timezones = document.getElementById("timezones");
+
+  fetch(`https://restcountries.com/v3.1/name/${userInput}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((obj) => {
+        console.log(obj);
+        flagImg.src = obj.flags.png;
+        name.innerText = obj.name.common;
+        officialName.innerText = obj.name.official;
+
+        region.innerHTML = obj.name.region;
+        population.innerText = obj.population;
+
+        area.innerText = obj.area;
+        timezones.innerText = obj.timezones;
+      });
+    });
+}
